ab878e971c1fe910f1fb2b4b02bfa5a4f4937c23
liufuwen <liufuwen@car-house.net>
2020-01-03 13:39:39 +0800

    修改清单配置，防止和别人的三方库冲突。

--- lib-filepaths/src/main/java/cn/carhouse/filepaths/AppFileProvider.java
+++ lib-filepaths/src/main/java/cn/carhouse/filepaths/AppFileProvider.java
@@ -15,7 +15,7 @@ import java.io.File;
 
 public class AppFileProvider {
 
-    public static final String FILE_PROVIDER = ".FileProvider";
+    public static final String FILE_PROVIDER = ".AppProvider";
 
     /**
      * 安装APK
